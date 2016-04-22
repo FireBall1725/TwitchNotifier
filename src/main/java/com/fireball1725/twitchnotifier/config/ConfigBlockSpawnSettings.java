d1ac4e199fb47f472be021c70e94acef7d709e06
FireBall1725 <fireball@fireball1725.com>
2016-04-21 21:25:53 -0700

    Updated to 1.9, added a bams feature!

---
+++ src/main/java/com/fireball1725/twitchnotifier/config/ConfigBlockSpawnSettings.java
@@ -0,0 +1,7 @@
+package com.fireball1725.twitchnotifier.config;
+
+/**
+ * Created by fireball1725 on 4/21/16.
+ */
+public class ConfigBlockSpawnSettings {
+}
---
+++ src/main/java/com/fireball1725/twitchnotifier/helper/SafeBlockReplacer.java
@@ -0,0 +1,7 @@
+package com.fireball1725.twitchnotifier.helper;
+
+/**
+ * Created by fireball1725 on 4/21/16.
+ */
+public class SafeBlockReplacer {
+}
